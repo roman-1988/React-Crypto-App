bd509b3e238e39935c1b5ac2ace47a57bb496413
Roman <roman-1@inbox.ru>
2024-01-26 14:14:25 +0300

    Редактировал функцию percentDifference для округления числа разницы. Реализовал логику округления чисел в количестве монет в компоненте AppSider. Там же создал условия если профит есть верстка с суммой будет отображаться зеленым цветом, а иначе красным цветом. Добавил тег с разницей в процентах с похожим функционалом.

--- react-crypto-app/src/components/layout/AppSider.jsx
+++ react-crypto-app/src/components/layout/AppSider.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { Layout, Card, Statistic, List, Typography, Spin } from "antd"
+import { Layout, Card, Statistic, List, Typography, Spin, Tag } from "antd"
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons"
 import { fakeFetchCrypto, fetchAssets } from "../../api"
 import { percentDifference } from "../../utils"
@@ -56,14 +56,26 @@ const AppSider = () => {
                         <List
                             size="small"
                             dataSource={[
-                                { title: "Total Profit", value: asset.totalProfit },
-                                { title: "Asset Amount", value: asset.amount },
+                                { title: "Total Profit", value: asset.totalProfit, withTag: true },
+                                { title: "Asset Amount", value: asset.amount, isPlain: true },
                                 { title: "Difference", value: asset.growPercent }
                             ]}
                             renderItem={(item) => (
                                 <List.Item>
                                     <span>{item.title}</span>
-                                    <span>{item.value}</span>
+                                    <span>
+                                        {item.withTag && (
+                                            <Tag color={asset.grow ? "green" : "red"}>
+                                                {asset.growPercent}%
+                                            </Tag>
+                                        )}
+                                        {item.isPlain && item.value}
+                                        {!item.isPlain && (
+                                            <Typography.Text type={asset.grow ? "success" : "danger"}>
+                                                {item.value.toFixed(2)}$
+                                            </Typography.Text>
+                                        )}
+                                    </span>
                                 </List.Item>
                             )}
                         />
