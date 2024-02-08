bca4e35e000049cf682c6a78972cbf4ad9227bed
Roman <roman-1@inbox.ru>
2024-02-08 17:47:13 +0300

    В компоненте AppContent написал логику подсчитывающую общую сумму количества всех видов крипты имеющуюся у нас в портфеле.

--- react-crypto-app/src/components/layout/AppContent.jsx
+++ react-crypto-app/src/components/layout/AppContent.jsx
@@ -1,4 +1,6 @@
-import { Layout } from "antd"
+import { Layout, Typography } from "antd"
+import SizeContext from "antd/es/config-provider/SizeContext"
+import { useCrypto } from "../../context/crypto-context"
 
 const contentStyle = {
     textAlign: "center",
@@ -9,9 +11,21 @@ const contentStyle = {
 }
 
 const AppContent = () => {
+    const { assets, crypto } = useCrypto()
+
+    const result = assets.map((asset) => {
+        const coin = crypto.find((c) => c.id === asset.id)
+        return asset.amount * coin.price
+    }).reduce((acc, val) => (acc += val), 0)
+        .toFixed(2)
+
     return (
         <>
-            <Layout.Content style={contentStyle}>Content</Layout.Content>
+            <Layout.Content style={contentStyle}>
+                <Typography.Title level={3} style={{ textAlign: "left", color: "#fff" }}>
+                    Portfolio: {result}$
+                </Typography.Title>
+            </Layout.Content>
         </>
     )
 }
--- react-crypto-app/src/components/layout/AppHeader.jsx
+++ react-crypto-app/src/components/layout/AppHeader.jsx
@@ -19,7 +19,7 @@ const AppHeader = () => {
     const [select, setSelect] = useState(false)
     const [modal, setModal] = useState(false)
     const [coin, setCoin] = useState(null)
-    const [drawer, setDrawer] = useState(true)
+    const [drawer, setDrawer] = useState(false)
     const { crypto } = useCrypto()
 
     useEffect(() => {
